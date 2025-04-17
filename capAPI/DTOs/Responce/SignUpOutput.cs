262d0bd51c83eab21d569ee9abfa849b6186f613
Enas Tailakh <enastailakh442@gmail.com>
2025-04-17 14:21:16 +0300

    signup

---
+++ capAPI/DTOs/Responce/SignUpOutput.cs
@@ -0,0 +1,11 @@
+﻿namespace capAPI.DTOs.Responce
+{
+    public class SignUpOutput
+    {
+       
+            public int UserId { get; set; }
+            public string Message { get; set; }
+        }
+
+    }
+
