314df00abadcb839e1a9fd3096cd5975ff0b5710
Enas Tailakh <enastailakh442@gmail.com>
2025-04-17 14:21:16 +0300

    signup

--- capAPI/Controllers/AuthController.cs
+++ capAPI/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Azure;
-
+using capAPI.Helpers;
 namespace capAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -52,11 +52,64 @@ namespace capAPI.Controllers
 
 
 
-                return StatusCode(200,$"Welcome {Response.Name}");
+                return StatusCode(200, $"Welcome {Response.Name}");
             }
             catch (Exception ex)
             {
                 return StatusCode(400, ex.Message);
+            }
+        }
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpInput input)
+        {
+            var response = new SignUpOutput();
+
+            try
+            {
+                if (string.IsNullOrEmpty(input.Email) || string.IsNullOrEmpty(input.Password) ||
+                    string.IsNullOrEmpty(input.FullName) || string.IsNullOrEmpty(input.Phone))
+                {
+                    throw new Exception("All fields are required");
+                }
+
+                Validation.IsValidEmail(input.Email);
+                Validation.IsValidPhone(input.Phone);
+                Validation.IsValidFullName(input.FullName);
+                Validation.IsValidPassword(input.Password);
+                Validation.IsValidBirthdate(input.Birthdate);
+
+                string conn = "Data Source=DESKTOP-CBGCB75;Initial Catalog=DBCapstone;Integrated Security=True;Trust Server Certificate=True;";
+                using (SqlConnection connection = new SqlConnection(conn))
+                {
+                    using (SqlCommand command = new SqlCommand("sp_RegisterClient", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        var nameParts = input.FullName.Split(' ');
+                        string firstName = nameParts[0];
+                        string lastName = nameParts.Length > 1 ? nameParts[1] : "";
+
+                        command.Parameters.AddWithValue("@FirstName", firstName);
+                        command.Parameters.AddWithValue("@LastName", lastName);
+                        command.Parameters.AddWithValue("@Email", input.Email);
+                        command.Parameters.AddWithValue("@PasswordHash", input.Password);
+                        command.Parameters.AddWithValue("@Phone", input.Phone);
+                        command.Parameters.AddWithValue("@Birthdate", input.Birthdate);
+
+
+                        connection.Open();
+                        var userId = command.ExecuteScalar();
+
+                        response.UserId = Convert.ToInt32(userId);
+                        response.Message = "User registered successfully";
+                    }
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(400, new { Message = ex.Message });
             }
         }
     }
