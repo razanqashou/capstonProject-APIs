5b0b5dee7f3d1f64c7d4eca9de90139c90854c64
razanqashou <razanqashou777@gmail.com>
2025-05-03 22:16:34 +0300

    send otp via email

--- capAPI/Controllers/AuthController.cs
+++ capAPI/Controllers/AuthController.cs
@@ -17,46 +17,71 @@ namespace capAPI.Controllers
         [Route("login")]
         public async Task<IActionResult> login(LoginInput input)
         {
-            var Response = new LoginOutput();
+           
             try
             {
+                string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+
+
                 if (string.IsNullOrEmpty(input.Email) || string.IsNullOrEmpty(input.Password))
                     throw new Exception("Invalid email or password");
 
-
-               string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
-
-              //  string conn = "Data Source=DESKTOP-CBGCB75;Initial Catalog=DBCapstone;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-
-
-                SqlConnection connection = new SqlConnection(conn);
-                connection.Open();
-                string query = @"SELECT userID, CONCAT(FirstName, ' ', LastName) AS FullName 
-                            FROM USERS 
-                           WHERE EMAIL = @email AND PasswordHash = @password AND RoleID = 3";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.CommandType = System.Data.CommandType.Text;
-                command.Parameters.AddWithValue("@email", input.Email);
-                command.Parameters.AddWithValue("@password", input.Password);
-                SqlDataAdapter adapter = new SqlDataAdapter(command);
-                DataTable dt = new DataTable();
-                adapter.Fill(dt);
-
-                if (dt.Rows.Count == 0)
-                    throw new Exception("invalid email / pass");
-                if (dt.Rows.Count > 1)
-                    throw new Exception("query contains more than one");
-
-
-                foreach (DataRow row in dt.Rows)
+                using (SqlConnection connection = new SqlConnection(conn))
                 {
-                    Response.Id = Convert.ToInt32(row["userID"]);
-                    Response.Name = row["FullName"].ToString();
+                    await connection.OpenAsync();
+
+                   
+                    string selectQuery = "SELECT TOP 1 * FROM USERS WHERE Email = @Email AND PasswordHash = @Password AND IsLoggedIn = 0 AND RoleID = 3";
+                    SqlCommand selectCommand = new SqlCommand(selectQuery, connection);
+                    selectCommand.Parameters.AddWithValue("@Email", input.Email);
+                    selectCommand.Parameters.AddWithValue("@Password", input.Password);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(selectCommand);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    if (dt.Rows.Count == 0)
+                        throw new Exception("invalid email / pass");
+                    if (dt.Rows.Count > 1)
+                        throw new Exception("query contains more than one");
+
+                    int userId = Convert.ToInt32(dt.Rows[0]["userID"]); 
+
+                    
+                    Random random = new Random();
+                    var otp = random.Next(11111, 99999).ToString();
+                    DateTime now = DateTime.Now;
+                    DateTime expiresAt = now.AddMinutes(5);
+
+                    
+                    string insertOtpQuery = @"
+                           INSERT INTO UserOTPCodes (UserID, OTPCode, CreatedAt, CreatedBy, ExpiresAt, IsUsed, isActive)
+                       VALUES (@UserID, @OTPCode, @CreatedAt, @CreatedBy, @ExpiresAt, 0, 1)";
+
+
+                    SqlCommand insertCommand = new SqlCommand(insertOtpQuery, connection);
+                    insertCommand.Parameters.AddWithValue("@UserID", userId);
+                    insertCommand.Parameters.AddWithValue("@OTPCode", otp);
+                    insertCommand.Parameters.AddWithValue("@CreatedAt", now);
+                    insertCommand.Parameters.AddWithValue("@CreatedBy", "System"); 
+                    insertCommand.Parameters.AddWithValue("@ExpiresAt", expiresAt);
+
+                    await insertCommand.ExecuteNonQueryAsync();
+                    try
+                    {
+                        await EmailHelper.SendOtpEmail(input.Email, "Your One-Time Sign-In Code", "We received a request to sign in to your account. Please use the code below to complete your sign-in process.", otp);
+                    }
+                    catch (Exception emailEx)
+                    {
+
+                        throw new Exception("Registration failed during email sending: " + emailEx.Message);
+                    }
+
+
+                    return StatusCode(200, "  Please check your email OTP has been sent!");
                 }
+ 
 
-
-
-                return StatusCode(200, $"Welcome {Response.Name}");
             }
             catch (Exception ex)
             {
@@ -77,7 +102,6 @@ namespace capAPI.Controllers
                     throw new Exception("Invalid email");
 
                 string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
-              //  string conn = "Data Source=DESKTOP-CBGCB75;Initial Catalog=DBCapstone;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
 
                 using (SqlConnection connection = new SqlConnection(conn))
                 {
@@ -92,28 +116,28 @@ namespace capAPI.Controllers
                         throw new Exception("No such email with RoleID 3.");
 
                     int userId = Convert.ToInt32(userIdObj);
-                    string otpCode = OTPHelper.GenerateOTP();
+                //    string otpCode = OTPHelper.GenerateOTP();
 
-                    string insertOtpQuery = @"
-                INSERT INTO UserOTPCodes (UserID, OTPCode, ExpiresAt)
-                VALUES (@UserID, @OTPCode, DATEADD(HOUR, 1, GETDATE()))";
+                //    string insertOtpQuery = @"
+                //INSERT INTO UserOTPCodes (UserID, OTPCode, ExpiresAt)
+                //VALUES (@UserID, @OTPCode, DATEADD(HOUR, 1, GETDATE()))";
 
-                    SqlCommand insertOtpCmd = new SqlCommand(insertOtpQuery, connection);
-                    insertOtpCmd.Parameters.AddWithValue("@UserID", userId);
-                    insertOtpCmd.Parameters.AddWithValue("@OTPCode", otpCode);
+                //    SqlCommand insertOtpCmd = new SqlCommand(insertOtpQuery, connection);
+                //    insertOtpCmd.Parameters.AddWithValue("@UserID", userId);
+                //    insertOtpCmd.Parameters.AddWithValue("@OTPCode", otpCode);
 
-                    int result = insertOtpCmd.ExecuteNonQuery();
+                //    int result = insertOtpCmd.ExecuteNonQuery();
 
-                    if (result > 0)
-                    {
-                        response.UserId = userId;
-                        response.OTPCode = otpCode;
-                        return StatusCode(201, response); 
-                    }
-                    else
-                    {
+                //    if (result > 0)
+                //    {
+                //        response.UserId = userId;
+                //        response.OTPCode = otpCode;
+                //        return StatusCode(201, response); 
+                //    }
+                //    else
+                //    {
                         return StatusCode(400, "Failed to send OTP code");
-                    }
+                    //}
                 }
             }
             catch (Exception ex)
@@ -144,18 +168,28 @@ namespace capAPI.Controllers
                 Validation.IsValidBirthdate(input.Birthdate);
 
                 string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
-               // string conn = "Data Source=DESKTOP-CBGCB75;Initial Catalog=DBCapstone;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
 
                 using (SqlConnection connection = new SqlConnection(conn))
                 {
                     connection.Open();
+
+                   
+                    using (SqlCommand checkEmailCmd = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Email = @Email", connection))
+                    {
+                        checkEmailCmd.Parameters.AddWithValue("@Email", input.Email);
+                        int existingCount = (int)await checkEmailCmd.ExecuteScalarAsync();
+                        if (existingCount > 0)
+                            throw new Exception("Email already registered");
+                    }
+
                     using (SqlCommand command = new SqlCommand("sp_RegisterClient", connection))
                     {
                         command.CommandType = CommandType.StoredProcedure;
 
+                      
                         var nameParts = input.FullName.Split(' ');
                         string firstName = nameParts[0];
-                        string lastName = nameParts.Length > 1 ? nameParts[1] : "";
+                        string lastName = nameParts.Length > 1 ? string.Join(" ", nameParts.Skip(1)) : "";
 
                         command.Parameters.AddWithValue("@FirstName", firstName);
                         command.Parameters.AddWithValue("@LastName", lastName);
@@ -164,12 +198,38 @@ namespace capAPI.Controllers
                         command.Parameters.AddWithValue("@Phone", input.Phone);
                         command.Parameters.AddWithValue("@Birthdate", input.Birthdate);
 
+                        int userId = Convert.ToInt32(await command.ExecuteScalarAsync());
 
+                        Random random = new Random();
+                        var otp = random.Next(11111, 99999).ToString();
+                        DateTime now = DateTime.Now;
+                        DateTime expiresAt = now.AddMinutes(5);
 
-                        int userId = Convert.ToInt32(command.ExecuteScalar());
+                        string insertOtpQuery = @"
+                    INSERT INTO UserOTPCodes (UserID, OTPCode, CreatedAt, CreatedBy, ExpiresAt, IsUsed, isActive)
+                    VALUES (@UserID, @OTPCode, @CreatedAt, @CreatedBy, @ExpiresAt, 0, 1)";
 
-                        response.UserId = Convert.ToInt32(userId);
-                        response.Message = "User registered successfully";
+                        SqlCommand insertCommand = new SqlCommand(insertOtpQuery, connection);
+                        insertCommand.Parameters.AddWithValue("@UserID", userId);
+                        insertCommand.Parameters.AddWithValue("@OTPCode", otp);
+                        insertCommand.Parameters.AddWithValue("@CreatedAt", now);
+                        insertCommand.Parameters.AddWithValue("@CreatedBy", "System");
+                        insertCommand.Parameters.AddWithValue("@ExpiresAt", expiresAt);
+
+                        await insertCommand.ExecuteNonQueryAsync();
+
+                        try
+                        {
+                            await EmailHelper.SendOtpEmail(input.Email, "Your One-Time Sign-Up Code", "We received a request to sign Up to your account. Please use the code below to complete your sign-upn process.", otp);
+                        }
+                        catch (Exception emailEx)
+                        {
+                            
+                            throw new Exception("Registration failed during email sending: " + emailEx.Message);
+                        }
+
+                        response.UserId = userId;
+                        response.Message = "Verifying your email using OTP";
                     }
                 }
 
@@ -197,9 +257,8 @@ namespace capAPI.Controllers
                     return BadRequest(new VerifyOtpOutput { Message = "Invalid password format" });
 
 
-               // string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
 
-                string conn = "Data Source=DESKTOP-CBGCB75;Initial Catalog=DBCapstone;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+                string conn = "Server=MSI\\SQLEXPRESS13;Database=lastupdateCapstonDB.bacpac;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
 
                 using (SqlConnection connection = new SqlConnection(conn))
                 {
