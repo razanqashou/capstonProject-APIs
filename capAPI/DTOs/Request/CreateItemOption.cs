6f1ddbffcabff7455b3ce3da17b391b90470e8e1
razanqashou <razanqashou777@gmail.com>
2025-05-06 21:24:14 +0300

    Implement itemOption

---
+++ capAPI/DTOs/Request/CreateItemOption.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace capAPI.DTOs.Request
+{
+    public class CreateItemOption
+    {
+       
+      
+       
+        public int ItemId { get; set; }
+
+       
+        public string NameAr { get; set; } 
+
+        public string NameEn { get; set; } 
+
+      
+        public int OptionCategoryId { get; set; }
+
+        public bool? IsRequired { get; set; }
+
+        public decimal? PriceAfterDiscount { get; set; }
+        public int? Quantity { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+
+      
+       
+    
+
+       
+   
+
+    }
+}
---
+++ capAPI/DTOs/Request/UpdateItemOption.cs
@@ -0,0 +1,37 @@
+﻿namespace capAPI.DTOs.Request
+{
+    public class UpdateItemOption
+    {
+        public int OptionId { get; set; }
+
+
+        public int ItemId { get; set; }
+
+
+        public string? NameAr { get; set; } = null!;
+
+        public string? NameEn { get; set; } = null!;
+
+
+        public int? OptionCategoryId { get; set; }
+
+        public bool? IsRequired { get; set; }
+
+
+        public decimal? PriceAfterDiscount { get; set; }
+
+        public int? Quantity { get; set; }
+
+
+
+
+
+        public string? UpdatedBy { get; set; }
+
+   
+
+
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
