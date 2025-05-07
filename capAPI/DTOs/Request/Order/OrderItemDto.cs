b6acc1ef81d170ffff3af7fb86e7943b6b78100b
razanqashou <razanqashou777@gmail.com>
2025-05-07 23:35:35 +0300

    Implement order item cycle

---
+++ capAPI/DTOs/Request/Order/OrderItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace capAPI.DTOs.Request.Order
+{
+    public class OrderItemDto
+    {
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public string CreatedBy { get; set; }
+
+      
+    }
+}
---
+++ capAPI/DTOs/Request/Order/UpdateOrderItem.cs
@@ -0,0 +1,14 @@
+﻿namespace capAPI.DTOs.Request.Order
+{
+    public class UpdateOrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+
+
+        public int? ItemId { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? DiscountValue { get; set; }
+    }
+}
